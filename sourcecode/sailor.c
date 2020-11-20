bcb2905abf04ee6a0378cfdc694d76b650547bc6
vamsi krishna <vamsimkota@gmail.com>
2020-11-20 18:31:39 +0530

    class_17

--- sourcecode/sailor.c
+++ sourcecode/sailor.c
@@ -1,3 +1,15 @@
+/*
+  Captain want to select one sailor sailors positioned from 1 to n. 
+  he is selecting even number positions until he get one sailor
+  example:
+  n = 9
+  1: 1 2 3 4 5 6 7 8 9
+  2: 2 4 6 8
+  3: 4 8
+  4: 8
+  output: 8
+*/
+
 #include<stdio.h>
 #include<math.h>
 int main(){

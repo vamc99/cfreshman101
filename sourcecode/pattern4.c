fb7a55b6f6378f4c1b17a8f3fe7a7b485e04e244
vamsi krishna <vamsimkota@gmail.com>
2020-11-25 09:34:59 +0530

    class 19

---
+++ sourcecode/pattern4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++)
+  {
+    if (i == 1 || i == n)
+    {
+      for (int j = 1; j <= n; j++)
+        printf("* ");
+    }
+    else
+    {
+      for (int j = 1; j <= n; j++)
+      {
+        if (j == 1 || j == n)
+          printf("* ");
+        else
+          printf("  ");
+      }
+    }
+    printf("\n");
+  }
+}

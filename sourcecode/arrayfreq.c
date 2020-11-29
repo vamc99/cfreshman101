15fd81a95a0bbe50d6a90ca41527b7e973502a96
vamsi krishna <vamsimkota@gmail.com>
2020-11-29 11:24:27 +0530

    class_22

---
+++ sourcecode/arrayfreq.c
@@ -0,0 +1,44 @@
+/*
+a[] = {1,2,1,2,3,4,5,4,3,2,4,5,7,9,100}
+out put
+1 -> 2
+2 -> 3
+3 -> 2
+4 -> 3
+5 -> 2
+7 -> 1
+9 -> 1
+100 -> 1
+*/
+
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+  /* code */
+  int n, max=INT_MIN;
+  scanf("%d",&n);
+  int a[n];
+  for (int i = 0; i < n; i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(int i=0;i<n;i++){
+    if(max<a[i])
+      max = a[i];
+  }
+  int freq[max+1];
+  for(int i=0;i<max+1;i++){
+    freq[i] = 0;
+  }
+  for (int i = 0; i < n; i++)
+  {
+    freq[a[i]]++;
+  }
+  for (int i = 0; i < max+1; i++)
+  {
+    if(freq[i]!=0)
+      printf("%d -> %d\n",i,freq[i]);
+  }
+  return 0;
+}
---
+++ sourcecode/arrayrotate.c
@@ -0,0 +1,31 @@
+/*
+  Left rotation 
+  read n 
+  a = {1,2,3,4,5}
+  rotate 2
+  output
+  {3,4,5,1,2}
+*/
+#include<stdio.h>
+int main(){
+  int n,r;
+  scanf("%d",&n);
+  int a[n];
+  for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+  }
+  scanf("%d",&r);
+  int b[r],c[n];
+  for(int i=0;i<r;i++){
+    b[i] = a[i];
+  }
+  for(int i=r,j=0; i<n;i++,j++){
+    c[j]=a[i];
+  }
+  for(int i=0,j = n-r;i<r;i++,j++){
+	  c[j] = b[i];
+  }
+  for(int i=0;i<n;i++){
+    printf("%d ",c[i]);
+  }
+}
